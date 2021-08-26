bb357b8cf5704b17d115575142b25dc5a5238f2f
Ethan Riegsecker <eriegsecker2@huskers.unl.edu>
2021-08-26 17:42:57 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author:Ethan Riegsecker (eriegsecker2@huskers.unl.edu)
+ *
+ * Date:08/26/2021
+ *
+ * Working with github to push and commit code
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Ethan Riegseecker!\n");
+    printf("Computer Science\n");
+
+    return 0;
+}
